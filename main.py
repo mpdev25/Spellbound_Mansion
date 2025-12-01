223c5f266c60e948c7c65e82919d2b94597bab4e
mpdev25 <michaelpeake123@gmail.com>
2025-11-30 16:06:19 -0800

    initial work on main

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import tkinter as tk
 
+
+
+def start_new_game():
+    while True:
+        user_input = input("Start a new game? y/n ").lower()
+        if user_input == "y":
+            return True
+        if user_input == "n":
+            return False
+        else:
+            print("Invalid input, please enter y or n.")
+    
 
 def main():
-
-
-
+    game_running = True
+    while game_running:
+        game_state = intro() 
+        game_running = start_new_game()
 
 
 
